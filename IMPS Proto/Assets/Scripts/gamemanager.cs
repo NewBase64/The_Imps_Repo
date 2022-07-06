6ee87713883f93a07fe5f0962a81285ff530ca63
Guilherme Silva <91626677+gigamob12@users.noreply.github.com>
2022-07-06 11:56:06 -0400

    UI
    First time commiting the UI

---
+++ IMPS Proto/Assets/Scripts/gamemanager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gamemanager : MonoBehaviour
+{
+    public static gamemanager instance;
+    public GameObject player;
+    public playerController playerScript;
+
+    public GameObject pauseMenu;
+    public GameObject playerDeadMenu;
+    public GameObject playerDamageFlash;
+
+    public Image HPBar;
+
+    public bool paused = false;
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<playerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel") && !playerDeadMenu.activeSelf)
+        {
+            if (!paused)
+            {
+                paused = true;
+                pauseMenu.SetActive(true);
+                Time.timeScale = 0;
+                Cursor.lockState = CursorLockMode.Confined;
+                Cursor.visible = true;
+            }
+            else
+            {
+                resume();
+            }
+                
+        }
+
+
+
+    }
+    public void resume()
+    {
+        paused = false;
+        pauseMenu.SetActive(false);
+        playerDeadMenu.SetActive(false);
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    public void playerDead()
+    {
+        paused = true;
+        playerDeadMenu.SetActive(true);
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+    }
+
+
+
+}
